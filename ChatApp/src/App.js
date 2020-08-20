f885ba4ea19c525d154d9773a924c4b57b075f48
Akash <52118954+Akash-cyb3r@users.noreply.github.com>
2020-08-20 11:34:52 +0530

    Add files via upload

---
+++ ChatApp/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./App.css";
+import Sidewindow from "./Sidewindow";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app_body">
+        <Sidewindow />
+      </div>
+    </div>
+  );
+}
+
+export default App;
